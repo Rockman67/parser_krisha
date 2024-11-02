9288b062e01eaf3870c40979c431d4184714e221
Rockman67 <130305105+Rockman67@users.noreply.github.com>
2024-11-02 11:37:42 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 from cx_Freeze import setup, Executable
 import os
 
-# Определение пути к директории с исполняемым файлом
+# Определение пути к директории с приложением
 if getattr(sys, 'frozen', False):
     application_path = os.path.dirname(sys.executable)
 else:
@@ -10,14 +10,12 @@ else:
 
 # Определение файлов, которые должны быть включены
 include_files = [
-    # Если у вас есть дополнительные файлы, добавьте их сюда
-    # Например, иконка:
     ('parser.ico', 'parser.ico')
 ]
 
 # Определение опций для cx_Freeze
 build_exe_options = {
-    "packages": ["os", "sys", "sqlite3", "requests", "bs4", "pyperclip", "logging", "re", "shutil", "threading", "queue", "datetime", "tkinter", "urllib"],
+    "packages": ["os", "sys", "sqlite3", "requests", "bs4", "pyperclip", "logging", "re", "shutil", "threading", "queue", "datetime", "tkinter", "urllib", "webbrowser"],
     "include_files": include_files,
     "excludes": []
 }
@@ -28,9 +26,9 @@ if sys.platform == "win32":
     base = "Win32GUI"  # Для GUI-приложений на Tkinter
 
 setup(
-    name = "Парсер Кришa",
+    name = "Парсер Krisha",
     version = "1.0",
-    description = "Программа для парсинга Кришa",
+    description = "Программа для парсинга Krisha",
     options = {"build_exe": build_exe_options},
     executables = [Executable("parser_gui.py", base=base, icon="parser.ico")]
 )
